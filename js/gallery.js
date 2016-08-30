6ec8748861e3565e8b1da320ba24b2604c63d036
BillyCostigan <leszek.wojciech@0net.pl>
2016-08-30 12:54:44 +0200

    almost done gallery

--- js/gallery.js
+++ js/gallery.js
@@ -25,16 +25,16 @@ change_bottom_alternate.addEventListener('click', function() {
 
     // slider_objects[i].classList.add('large_img');
     // slider_objects[j].classList.add('translate_bottom');
-    slider_objects[j].classList.add('hide_me');
+    slider_objects[j].classList.add('hide_me');//pierwszy u gory widoczny film chowamy
 
     // slider_objects[i+1].classList.remove('large_img');
     // slider_objects[(j-1+10)%10].classList.add('translate_bottom');
 
     // slider_objects[(j-2+10)%10].classList.add('translate_bottom');
-    slider_objects[j+3].classList.remove('hide_me');
+    slider_objects[j+3].classList.remove('hide_me');//pierwszy u dolu niewidoczny film pokazujemy
 
 
-    console.log(j);
+    // console.log(j);
     if (j >= 6) {
         // console.log(change_bottom);
         change_bottom.classList.remove('hide_me');
@@ -42,38 +42,40 @@ change_bottom_alternate.addEventListener('click', function() {
         // console.log(change_top_alternate);
         change_top.classList.add('hide_me');
         change_top_alternate.classList.remove('hide_me');
-        console.log('top1');
-        j = j % 6;
-    } else if (j >= 0) {
+        console.log(j + ' w 1 bot');
+        // j = j % 6;
+    } else if (j > 0) {
         change_top.classList.add('hide_me');
         change_top_alternate.classList.remove('hide_me');
         change_bottom.classList.add('hide_me');
         change_bottom_alternate.classList.remove('hide_me');
         j = j + 1;
-        console.log('top2');
+        console.log(j + ' w 3 bot');
     } else if (j === 0) {
         change_top.classList.add('hide_me');
         change_top_alternate.classList.remove('hide_me');
-        console.log('top4');
-    } else {
         j = j + 1;
-        console.log('top3');
+        console.log(j);
+    } else {
+        console.log('coś poszło nie tak');
     }
-
 })
 
 // console.log(j);
 
-var i = 6;
+// console.log(j);
+
+
 
 change_top_alternate.addEventListener('click', function() {
-    console.log(i);
+    console.log(j);
+    // j = j + 6;
 
     // slider_objects[i].classList.add('large_img');
     // slider_objects[i].classList.add('translate_top');
-    slider_objects[i].classList.remove('hide_me');
+    slider_objects[j-1].classList.remove('hide_me');//pierwszy u gory niewidoczny film pokazujemy
     // console.log(slider_objects[i]);
-    slider_objects[i+3].classList.add('hide_me');
+    slider_objects[j+2].classList.add('hide_me');//pierwszy u dolu widoczny film chowamy
     // console.log(slider_objects[i+3]);
     // console.log(slider_objects[i]);
     // i = i + 1;
@@ -84,27 +86,52 @@ change_top_alternate.addEventListener('click', function() {
     // console.log(slider_objects[i-3]);
     // slider_objects[i-3].classList.remove('hide_me');
 
-    if (i >= 6)  {
+    if (j >= 6)  {
         change_bottom.classList.add('hide_me');
         change_bottom_alternate.classList.remove('hide_me');
-        console.log('>=6');
-        i = i - 1;
-    } else if (i > 0) {
+        console.log(j + ' w 1 top');
+        j = j - 1;
+    } else if (j > 0) {
          change_bottom.classList.add('hide_me');
          change_bottom_alternate.classList.remove('hide_me');
-         console.log('>0');
-         i = i - 1;
-    } else {
+         console.log(j + ' w 2 top');
+         j = j - 1;
+    } else if (j === 0) {
         change_top.classList.remove('hide_me');
         change_top_alternate.classList.add('hide_me');
         change_bottom.classList.add('hide_me');
         change_bottom_alternate.classList.remove('hide_me');
-        console.log('beforelast');
-        i = i + 6;
+        console.log(j + ' w 3 top');
+        // j = j + 6;
+    } else {
+        console.log('nie działa');
     }
-    console.log(i);
+    // console.log(j);
     // i = i - 1;
 
 })
+
+// change_top_alternate.addEventListener('click', function() {
+//
+//     change_bottom_alternate.addEventListener('click', function() {
+//
+//         i = i - 1;
+//
+//     })
+//
+// })
+//
+// change_bottom_alternate.addEventListener('click', function() {
+//
+//     change_top_alternate.addEventListener('click', function() {
+//
+//         j = j - 1;
+//
+//     })
+//
+// })
+
+
+
 
 });
